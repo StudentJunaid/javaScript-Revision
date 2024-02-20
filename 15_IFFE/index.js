278390965bb32a94745eb9fecb79ff1215f0c9c0
Junaid Noor <junaidkhan97991@gmail.com>
2024-02-20 07:30:41 -0800

    IFFE

---
+++ 15_IFFE/index.js
@@ -0,0 +1,17 @@
+// Immediately invoked Function Expression (IFFE)
+
+// why we need to use IFFE?
+// Ans: global scop se problem hoti hai kyi bar us global scop ke jo bhi varibles decleration hai usko hatanay k lye hum IFFE ka use krtay hai
+
+// Example of IFFE 
+
+(function chai(){
+    // Named IFFE
+    console.log(`DB CONNECTED`)
+})(); // we need to use semicolen aftter IFFE if we need to use another IFFE and it's necessery
+
+((name)=>{
+    // UnNamed IFFE
+    console.log(`DB CONNECTED Two ${name}`)
+})("Cherry")
+
