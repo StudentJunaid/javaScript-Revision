16b4a015fd51896d19f65254ce08dfae315af4ab
Junaid Noor <junaidkhan97991@gmail.com>
2024-02-20 00:11:03 -0800

    function revision

--- 12_Functions/index.js
+++ 12_Functions/index.js
@@ -9,8 +9,28 @@ function saymyname(){
 }
 // saymyname()
 
+// function addTwonumber(number1,number2){
+//     console.log(number1 + number2)
+// }
+
+
 function addTwonumber(number1,number2){
-    console.log(number1 + number2)
+    // let result = number1 + number2 
+    // return result
+    return number1 + number2
 }
 
-addTwonumber(2,null)
+
+const result = addTwonumber(2,5)
+// console.log("Result: ",result)
+
+function userloggedIn(username = "jerry"){
+    if(!username){
+        console.log("please enter a user code")
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(userloggedIn('jerry'))
+console.log(userloggedIn())
